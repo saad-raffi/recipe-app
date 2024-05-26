29efe6d8bfb117f60d995ea113f83657431ff727
Saad Mohammad <saadmec1@gmail.com>
2024-05-26 15:24:58 +0600

    Home page changes

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -12,11 +12,10 @@ export default function Banner() {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-start text-neutral-content">
         <div className="max-w-md text-center">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <h1 className="mb-5 text-5xl font-bold">Welcome to </h1>
+          <h2 className="mb-5 text-5xl font-bold">Delicious Recipes</h2>
           <p className="mb-5 ">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+          Welcome to Delicious Recipes, your one-stop destination for all things culinary! Whether you are a seasoned chef, a cooking enthusiast, or a beginner in the kitchen, our platform offers a diverse collection of recipes, cooking tips, and inspiration to elevate your culinary skills.
           </p>
           <button className="btn btn-primary">Get Started</button>
         </div>
